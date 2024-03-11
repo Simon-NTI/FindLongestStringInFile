f294c8929bd6b93fda02cc0cd2f3b6ea2449556c
Simon-NTI <simon.hall@elev.ga.nti.gse>
2024-03-11 09:17:38 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FindLongestStringInFile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string? path = Console.ReadLine();
+        string longestString = "";
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    if(line.Length > longestString.Length)
+                    {
+                        Console.WriteLine(line);
+                        longestString = line;
+                    }
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Could not read file");
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("The longest string found was:");
+            Console.WriteLine(longestString);
+        }
+    }
+}
